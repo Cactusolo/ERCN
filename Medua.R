89d72201f4558c6137239a56641d706f116bcdf0
Miao <cactus@ufl.edu>
2018-03-21 14:08:58 -0400

    adding figure

--- Medua.R
+++ Medua.R
@@ -6,7 +6,7 @@ library("MEDUSA")
 
 richness <- read.csv("data/Genus.Richness.csv", header=TRUE)
 tree <- read.tree("results/Date_CN_ToL_genus_pruned_mono.tre")
-is.ultrametric(tree)
+is.ultrametric(tree) #the phy tree need to be ultrametric
 
 library("phytools")
 
@@ -14,16 +14,20 @@ tree.ult <- force.ultrametric(tree, method="extend")
 tree.nnl <- force.ultrametric(tree, method="nnls")
 is.ultrametric(tree.ult)
 write.tree(tree.ult, "results/Dated_CN_ToL_genus_pruned_mono_ultra.tre")
+
 par(mfrow=c(1,3))
 plot.phylo(tree, show.tip.label = FALSE, main="Orignal")
 plot.phylo(tree.ult, show.tip.label = FALSE, main="extend")
 plot.phylo(tree.nnl, show.tip.label = FALSE, main="nnls")
 
-rosids_medusa <- MEDUSA(phy=tree.ult, richness = richness)
-rosids_medusa2 <- MEDUSA(phy=tree, richness = richness,criterion="aic",stop="threshold")
-
-summ <- medusaSummary(rosids_medusa)
-summ2 <- medusaSummary(rosids_medusa2)
+#CNTree_medusa <- MEDUSA(phy=tree.ult, richness = richness)
+CNTree_medusa <- readRDS("results/CNTree_MEDUSA.rds")
+summ <- medusaSummary(CNTree_medusa)
 
 saveRDS(rosids_medusa,"results/MEDUSA.rds")
 saveRDS(sum,"results/CNTree_MEDUSA_sum.rds")
+
+#this figure is till ugly
+plotPrettyTree(summ, show.tip.label = FALSE, time=FALSE, node.labels = TRUE)
+axisPhylo()
+
